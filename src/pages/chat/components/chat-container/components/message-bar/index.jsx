db9fb7a496f0ee1d869649ecb3df7a9682b49690
sathish <sathish958527@gmail.com>
2025-02-04 20:22:16 +0530

    Update index.jsx

--- src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -116,8 +116,8 @@ const MessageBar = () => {
   };
 
   return (
-    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 gap-6 mb-5">
-      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5">
+    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 gap-2 mb-5">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-2 pr-5">
         <input
           type="text"
           className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none"
